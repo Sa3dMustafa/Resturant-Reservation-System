2ac0178f984e651e5db677aedc63fdb419aceac5
abdosaad203 <abdosaad4900@gmail.com>
2026-08-13 03:19:18 +0300

    fix next standalone output

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,15 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
   },
 };
 
